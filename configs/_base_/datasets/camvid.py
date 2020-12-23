158cd0ecae0aaa5c1896c7b0c199afe0bb1f7ec4
jpl <513963138@qq.com>
2020-12-23 08:58:17 +0800

    camvid fix

--- configs/_base_/datasets/camvid.py
+++ configs/_base_/datasets/camvid.py
@@ -3,16 +3,17 @@ dataset_type = 'CamVidDataset'
 data_root = 'data/CamVid'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-img_scale = (480, 360)
+img_scale = (960, 720)
+crop_size = (960, 720)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
     dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
-    dict(type='RandomCrop', crop_size=img_scale, cat_max_ratio=0.75),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=img_scale, pad_val=0, seg_pad_val=255),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg']),
 ]
--- mmseg/datasets/camvid.py
+++ mmseg/datasets/camvid.py
@@ -5,9 +5,9 @@ from .custom import CustomDataset
 @DATASETS.register_module()
 class CamVidDataset(CustomDataset):
     CLASSES = ['Sky', 'Building', 'Pole', 'Road', 'Pavement', 'Tree', 'SignSymbol', 'Fence', 'Car', 'Pedestrian',
-               'Bicyclist', 'Unlabelled']
+               'Bicyclist']
     PALETTE = [[128, 128, 128], [128, 0, 0], [192, 192, 128], [128, 64, 128], [60, 40, 222], [128, 128, 0],
-               [192, 128, 128], [64, 64, 128], [64, 0, 128], [64, 64, 0], [0, 128, 192], [0, 0, 0]]
+               [192, 128, 128], [64, 64, 128], [64, 0, 128], [64, 64, 0], [0, 128, 192]]
 
     def __init__(self, **kwargs):
         super(CamVidDataset, self).__init__(
